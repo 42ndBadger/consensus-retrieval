0ec9cf8f4153525661a0fe5347f68f23e7259334
Dominik Rosch <dominik.rosch@student.kit.edu>
2026-07-28 09:36:37 +0200

    add missing data_gen.rs file

---
+++ src/data_gen.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use std::hash::{Hash};
+use std::fmt::Debug;
+
+use ahash::RandomState;
+
+use crate::alias::AliasTable;
+
+pub fn from_value_distribution<V>(n: usize, distibution: &HashMap<&V, f64>) -> HashMap<u64, V>
+where
+    V: Copy + Eq + Hash + Debug,
+{
+    let hasher = RandomState::new();
+    let mut result = HashMap::new();
+
+    let alias_table = AliasTable::from(distibution).unwrap();
+    for i in 0..n {
+        let mut key = hasher.hash_one(i);
+        let mut j = 0;
+        while result.contains_key(&key) {
+            key = hasher.hash_one(i + n*j);
+            j += 1;
+        }
+        let r = hasher.hash_one(key);
+        result.insert(key, *alias_table.sample(r));
+    }
+  result  
+}
