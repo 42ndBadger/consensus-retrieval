601d5d4258280b15064fdc7776c75e4bbf521d8e
Jonatan Ziegler <jonatan.ziegler@student.kit.edu>
2026-08-03 09:52:49 +0200

    more benches and fix aes instruction

--- examples/rand.rs
+++ examples/rand.rs
@@ -1,8 +1,10 @@
+use std::time::Instant;
+
 use consensus_retrieval::{ConsensusRetrieval, parameters::Parameters};
 use rand::{RngExt, SeedableRng, rngs::StdRng};
 
 fn main() {
-    let n = 1_000_000;
+    let n = 100_000;
     let sigma = 3;
     println!("n={n}, sigma={sigma}");
 
@@ -13,8 +15,11 @@ fn main() {
     let params = Parameters::new_from_raw(b, 1. / b as f64, 0.1);
     // let params = Parameters::new_like_in_proof(b);
 
+    let start = Instant::now();
     let retrieval =
         ConsensusRetrieval::new_with_parameters(&kv, params, ahash::RandomState::with_seed(11));
+    let took = start.elapsed();
+    println!("construction took {took:?}, {:?} per key", took / n);
     println!("overhead {}", retrieval.space_overhead());
     println!(
         "consensus size {}",
