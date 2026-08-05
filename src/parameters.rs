5ea4589aed106a24b58388d105a9636f0c3438a6
Jonatan Ziegler <jonatan.ziegler@student.kit.edu>
2026-08-05 17:13:56 +0200

    change default beta scale

--- src/parameters.rs
+++ src/parameters.rs
@@ -11,7 +11,7 @@ impl Parameters {
     pub fn new_like_in_proof(b: usize) -> Self {
         assert!(b > 0, "b must be positive");
         let ε = 1. / (b as f64 + 1.); // todo how to ensure < 1?
-        Self::new_from_raw(b, ε, 1.)
+        Self::new_from_raw(b, ε, 0.1)
     }
 
     pub fn new_from_raw(b: usize, ε: f64, beta_scale: f64) -> Self {
@@ -31,13 +31,13 @@ impl Parameters {
 
     pub fn new_from_raw_difficulty(b: usize, ε: f64, beta_scale: f64, max_diff: f64) -> Self {
         let β = (f64::ceil(beta_scale * (b as f64).sqrt() * (b as f64).log2()) as usize).max(1);
-       Self {
-           avg_group_load: (b as f64 + β as f64 / 2.) * (1. - ε ),
-           inital_group_width: b,
-           insertion_increment: β,
-           max_difficulty_of_task: max_diff,
-           max_difficulty_at_group_border: max_diff,
-       } 
+        Self {
+            avg_group_load: (b as f64 + β as f64 / 2.) * (1. - ε),
+            inital_group_width: b,
+            insertion_increment: β,
+            max_difficulty_of_task: max_diff,
+            max_difficulty_at_group_border: max_diff,
+        }
     }
 
     pub fn new_with_scales(b: usize, beta_scale: f64, eps_scale: f64) -> Self {
